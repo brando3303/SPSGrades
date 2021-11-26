b5d48e3ac755f4ef20f585bd32f4345c8241185f
Brandon Bailey <brando3303@gmail.com>
2021-11-26 00:23:06 -0800

    Fixes #25 - the CSS ID for the toolbar is currently TitleArea, feel free to change the color (although I did choose from the palette you suggested).

--- common/src/main/java/com/brandon/scraper/Utils.java
+++ common/src/main/java/com/brandon/scraper/Utils.java
@@ -1,8 +1,19 @@
 package com.brandon.scraper;
 
 import com.codename1.charts.util.ColorUtil;
+import com.codename1.ui.Form;
+import com.codename1.ui.plaf.Border;
+import com.codename1.ui.plaf.UIManager;
 
 
 public class Utils {
 
+    /**
+    * NOTE: Setting the color of a toolbar will not carry over after a Live CSS recompile
+    * */
+    public static void setToolbarUIIDForSolidColor(Form form, String UIID){
+        form.getToolbar().setSelectedStyle(UIManager.getInstance().getComponentStyle(UIID));
+        form.getToolbar().getUnselectedStyle().setBorder(Border.createEmpty(),true);
+        form.getToolbar().getUnselectedStyle().setBgTransparency(255);
+    }
 }
